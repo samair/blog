ccfaf6bdd8ae9513764bea98df197e597051586e
Sameer Chandra <tsameerc@gmail.com>
2025-07-18 09:49:34 +0530

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,7 +15,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-THEME = "./pelican-themes/Flex"
+THEME = "/home/sameer/actions-runner/_work/blog/blog//pelican-themes/Flex"
 
 # Blogroll
 LINKS = (
